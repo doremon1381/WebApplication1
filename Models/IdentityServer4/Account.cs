ca714b3a75fcb12e77fcefe5d0451271589443ed
doremon1381 <doremon1381@gmail.com>
2023-03-23 15:44:39 +0700

    - delete file.

--- Models/IdentityServer4/Account.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.Models.IdentityServer4
-{
-    /// <summary>
-    /// use for sending account information to web client
-    /// </summary>
-    public class Account
-    {
-    }
-}
